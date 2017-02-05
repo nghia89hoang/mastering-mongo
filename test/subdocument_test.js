8004a5964133440737024a9343c1413b8e6279ad
Nghe-HP <nghia89hoang@gmail.com>
2017-02-05 15:15:57 +0700

    - adding and removing subdocument

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -15,5 +15,41 @@ describe('Subdocuments', () => {
         done()
       })
   })
-  
+
+  it('can add subdocuments to an existing record', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: []
+    })
+    joe.save()
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        user.posts.push({title: 'A new post'})
+        return user.save()
+      })
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        assert(user.posts[0].title === 'A new post')
+        done()
+      })
+  })
+
+  it('can remove an existing subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{title: 'Post title'}]
+    })
+    joe.save()
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        const post = user.posts[0]
+        post.remove()
+        return user.save()
+      })
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        assert(user.posts.length === 0)
+        done()
+      })
+  })
 })
