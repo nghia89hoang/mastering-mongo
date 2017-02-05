89e89b09920520dd428fae8ffca50dbb25e04cad
Nghe-HP <nghia89hoang@gmail.com>
2017-02-05 10:15:18 +0700

    - Add postCount, testing mongo update operator

--- test/update_test.js
+++ test/update_test.js
@@ -4,7 +4,7 @@ const User = require('../src/user')
 describe('Updating records', () => {
   let joe
   beforeEach((done ) => {
-    joe = new User({name: 'Joe'})
+    joe = new User({name: 'Joe', postCount : 0})
     joe.save()
       .then(() => done())
   })
@@ -39,5 +39,14 @@ describe('Updating records', () => {
 
   it('A model class can find a record with an Id and update', (done) => {
     assertName(User.findByIdAndUpdate(joe._id, {name: 'Alex'}), done)
+  })
+
+  it('A user can have their postCount increment by 1', (done) => {
+    User.update({name: 'Joe'}, {$inc: {postCount: 1}})
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        assert(user.postCount === joe.postCount + 1)
+        done()
+      })
   })
 })
