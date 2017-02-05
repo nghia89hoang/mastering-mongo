fd7ce5079c5f73cc97e4e48b53ed9a9c8c49c2bf
Nghe-HP <nghia89hoang@gmail.com>
2017-02-05 21:09:09 +0700

    - test virtual type

--- test/update_test.js
+++ test/update_test.js
@@ -4,7 +4,7 @@ const User = require('../src/user')
 describe('Updating records', () => {
   let joe
   beforeEach((done ) => {
-    joe = new User({name: 'Joe', postCount : 0})
+    joe = new User({name: 'Joe', likes : 0})
     joe.save()
       .then(() => done())
   })
@@ -41,11 +41,11 @@ describe('Updating records', () => {
     assertName(User.findByIdAndUpdate(joe._id, {name: 'Alex'}), done)
   })
 
-  it('A user can have their postCount increment by 1', (done) => {
-    User.update({name: 'Joe'}, {$inc: {postCount: 1}})
+  it('A user can have their like increment by 1', (done) => {
+    User.update({name: 'Joe'}, {$inc: {likes: 1}})
       .then(() => User.findOne({name: 'Joe'}))
       .then((user) => {
-        assert(user.postCount === joe.postCount + 1)
+        assert(user.likes === joe.likes + 1)
         done()
       })
   })
