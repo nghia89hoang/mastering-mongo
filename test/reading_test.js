0d3be9712729ab05586078686393c970e6b7f4fd
Nghe-HP <nghia89hoang@gmail.com>
2017-02-07 17:32:30 +0700

    -test pagination

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,10 +2,15 @@ const assert = require('assert')
 const User = require('../src/user')
 
 describe('Reading user out of database', () => {
-  let joe
+  let joe, maria, alex, zach
   beforeEach((done) => {
     joe = new User({name: 'Joe'})
-    joe.save().then(() => done())
+    alex = new User({name: 'Alex'})
+    maria = new User({name: 'Maria'})
+    zach = new User({name: 'Zach'})
+
+    Promise.all([alex.save(), joe.save(), maria.save(), zach.save()])
+      .then(() => done())
   })
 
   it('Find all user with a name of "Joe"', (done) => {
@@ -20,6 +25,20 @@ describe('Reading user out of database', () => {
     User.findOne({_id : joe._id})
       .then((user) => {
         assert(user.name === joe.name)
+        done()
+      })
+  })
+
+  it('can skip and limit the result set', (done) => {
+    User.find({})
+      .sort({name: 1})
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        //not sure about order of users due to parallel save() above
+        assert(users.length === 2)
+        assert(users[0].name === 'Joe')
+        assert(users[1].name === 'Maria')
         done()
       })
   })
